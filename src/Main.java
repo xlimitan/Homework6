c1932211b85aa16c8f6136fcff3dc3c9beb4d13f
xlimitan <xlimitan@yandex.ru>
2023-02-13 12:34:36 +0300

    Задача № 1

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,14 @@ public class Main {
         task2();
     }
 
-    public static void task1 () {
+    public static void task1() {
         System.out.println("Задача 1");
-        // Пишем код для задачи 1
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Итерация цикла " + i);
+        }
     }
 
-    public static void task2 () {
+    public static void task2() {
         System.out.println("Задача 2");
         // Пишем код для задачи 2
     }

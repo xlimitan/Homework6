524f70543198494d0bbca57d56f10da73b1d0b33
xlimitan <xlimitan@yandex.ru>
2023-02-13 12:39:45 +0300

    Задача № 2.1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,14 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+        task9();
+        task10();
     }
 
     public static void task1() {
@@ -16,5 +24,32 @@ public class Main {
         for (int i = 10; i >= 1; i--) {
             System.out.println("Итерация цикла " + i);
         }
+    }
+    public static void task3() {
+        System.out.println("Задача 3");
+        for (int i = 0; i <= 17; i += 2) {
+            System.out.println("Итерация цикла " + i);
+        }
+    }
+    public static void task4() {
+        System.out.println("Задача 4");
+    }
+    public static void task5() {
+        System.out.println("Задача 5");
+    }
+    public static void task6() {
+        System.out.println("Задача 6");
+    }
+    public static void task7() {
+        System.out.println("Задача 7");
+    }
+    public static void task8() {
+        System.out.println("Задача 8");
+    }
+    public static void task9() {
+        System.out.println("Задача 9");
+    }
+    public static void task10() {
+        System.out.println("Задача 10");
     }
 }

41ed7f77d278445deba177e6faa908cdc28572c4
xlimitan <xlimitan@yandex.ru>
2023-02-13 12:49:10 +0300

    Задача № 7

--- src/Main.java
+++ src/Main.java
@@ -51,6 +51,9 @@ public class Main {
     }
     public static void task7() {
         System.out.println("Задача 7");
+        for (int i = 1; i <= 512; i *= 2) {
+            System.out.println("Итерация цикла " + i);
+        }
     }
     public static void task8() {
         System.out.println("Задача 8");
